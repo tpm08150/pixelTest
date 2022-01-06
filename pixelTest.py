3edde4f7c71047280d0f26b27d5bf9b74c4f3efb
tpm08150 <63214782+tpm08150@users.noreply.github.com>
2022-01-06 17:59:26 -0600

    Add files via upload

--- pixelTest.py
+++ pixelTest.py
@@ -2,6 +2,7 @@ import time
 import board
 import neopixel
 import RPi.GPIO as GPIO
+from pynput import keyboard
 
 
 LED_COUNT = 1000
@@ -21,4 +22,48 @@ for i in range(50):
     np.show()
 end = time.time()
 print(end-start)
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+def on_press(key):
+    charKeys = ['z', 'x']
+    x = 0
+    try:
+            
+        if key.char == charKeys[0]:
+            r += 1
+        if key.char == charKeys[1]:
+            r -= 1
+            
+        for i in range(50):
+            np[i] = (r, 0, 0)
+            np.show()
+end = time.time()
+            
+    except:
+        pass
+    
+
+def on_release(key):
+    global oct
+
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+    if key == keyboard.Key.up:
+        pass
+    if key == keyboard.Key.down:
+        pass
+    if key == keyboard.Key.space:
+        pass
+        
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
